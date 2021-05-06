e5ec69169a95b63dfcb15619542e603b05b14040
Dima <krepakdima@gmail.com>
2021-05-06 14:59:50 +0300

    heroku check

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,12 +1,39 @@
+import { useEffect, useState } from 'react';
 import Post from '../post/Post';
 import Share from '../share/Share';
 import './feed.css';
+import axios from 'axios'
 
 export default function Feed() {
+    const [posts, setPosts] = useState([])
+
+    const fetchPosts = async () => {
+        try {
+            const res = await axios.get(`/api/posts/timeline/all`, {
+                headers: {
+                    'Auth': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDkwMzkxMWQ5NzQ0YWQxM2M1NTEwZTciLCJpYXQiOjE2MjAwNjQ1Mjl9.foE4UJf9wfNlTrUpRrkX8erczdyxygHj4dJ2u7ifca4`
+                }
+            });
+            console.log(res.data);
+            setPosts(res.data)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        fetchPosts();
+    }, [])
     return (
         <div className="feed-container">
             <Share />
-            <Post />
+            {posts.map((p) => {
+                return (
+                    <Post
+                        key={p._id}
+                        post={p}
+                    />
+                )
+            })}
         </div>
     )
 }
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,30 +1,38 @@
 import './post.css'
 import { DeleteOutline, ThumbUp } from "@material-ui/icons"
-export default function Post() {
+import { format } from 'timeago.js'
+
+export default function Post({ post }) {
     return (
         <div className="post">
             <div className="post-wrapper">
                 <div className="post__top">
                     <div className="post__top-left">
-                        <img className="post-profile-picture" src='/assets/profile/pic1.jpeg' alt='' />
-                        <span className="post-username">Dima Krepak</span>
-                        <span className="post-date">10 min ago</span>
+                        <img className="post-profile-picture"
+                            src={post.owner.avatar || '/assets/profile/noavatar.png'}
+                            alt=''
+                        />
+                        <span className="post-username">{post.username}</span>
+                        <span className="post-date">{format(post.createdAt)}</span>
                     </div>
                     <div className="post__top-right">
                         <DeleteOutline className="post-icon" />
                     </div>
                 </div>
                 <div className="post__center">
-                    <span className="post-text">Hellllo First!!asd nasd asdsa  sad</span>
-                    <img className="post-image" src="assets/post.jpg" alt="" />
+                    <span className="post-text">{post.body}</span>
+                    <img className="post-image"
+                        src={"assets/post.jpg"}
+                        alt=""
+                    />
                 </div>
                 <div className="post__bottom">
                     <div className="post__bottom-left">
                         <ThumbUp style={{ fill: '#068bff' }} className="post-icon" />
-                        <span className="post-like-counter">102 likes</span>
+                        <span className="post-like-counter">{post.likes.length} likes</span>
                     </div>
                     <div className="post__bottom-right">
-                        <span className="post-comment-text"> 32 Comments</span>
+                        <span className="post-comment-text">{post.comments.length} Comments</span>
                     </div>
                 </div>
             </div>
