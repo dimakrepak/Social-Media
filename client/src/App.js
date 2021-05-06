a730b014f4ee34d4fb7a578402f03d2acf09553a
Dima <krepakdima@gmail.com>
2021-05-06 12:28:13 +0300

    loggin add

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div>
-      <Home />
-      <Login />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/register">
+          Register
+        </Route>
+
+        <Route path="/profile/:username">
+          Profile
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -2,8 +2,20 @@ import './login.css'
 
 export default function Login() {
     return (
-        <div>
-            
+        <div className="login">
+            <div className="login__left"></div>
+            <div className="login__right">
+                <div className="login-box">
+                    <div className="login-box__top">
+                        <input type="text" placeholder="Email" className="login-input" />
+                        <input type="text" placeholder="Password" className="login-input" />
+                        <button className="login-button">Log in</button>
+                        <span className="login-forget">Forgot password?</span>
+                    </div>
+                    <button className="create-button">Create New Account</button>
+                </div>
+            </div>
+
         </div>
     )
 }
