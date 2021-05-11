116b4fa3fed1c6898c25f6621c1371c8a1abe716
Dima <krepakdima@gmail.com>
2021-05-11 15:40:36 +0300

    import image added

--- src/server.js
+++ src/server.js
@@ -11,33 +11,11 @@ const app = express();
 const port = process.env.PORT || 8000;
 dotenv.config()
 
-// const server = http.createServer(app);
-// const io = socketio(server);
-
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-  // Express serve up index.html file if it doesn't recognize routeget
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-// io.on('connection', () => console.log('new websocket connected'))
-
-//Connection to db with mongoose
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-})
-  .then(() => console.log('database connect'))
-  .catch(err => console.log(err))
-
-//middleware
+// express middleware
 app.use(cors());
 app.use(express.json());
 app.use('/api', appRouter);
+
 //multer middleware
 // const storage = multer.diskStorage({
 //   destination: (req, file, cb) => {
@@ -56,5 +34,30 @@ app.use('/api', appRouter);
 //     console.log(err);
 //   }
 // })
+
+// const server = http.createServer(app);
+// const io = socketio(server);
+// io.on('connection', () => console.log('new websocket connected'))
+
+//Connection to db with mongoose
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+  .then(() => console.log('database connect'))
+  .catch(err => console.log(err))
+
+//Connection to client side
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+  // Express serve up index.html file if it doesn't recognize routeget
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`application start at ${port}`));
