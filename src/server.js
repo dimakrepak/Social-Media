9b70c03be1e5a7c15c777d053f5c9da35aa4b56e
Dima <krepakdima@gmail.com>
2021-05-11 15:17:30 +0300

    import image added

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 //middleware
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
 app.use('/api', appRouter);
 //multer middleware
 // const storage = multer.diskStorage({
