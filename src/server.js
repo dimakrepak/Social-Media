457527426876038930d475d240c107a466af0a23
Dima <krepakdima@gmail.com>
2021-05-16 02:10:52 +0300

    follow added!

--- src/server.js
+++ src/server.js
@@ -16,29 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', appRouter);
 
-//multer middleware
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'public/images');
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, req.body.name);
-//   }
-// });
-// const upload = multer({ storage });
-
-// app.post('/api/upload', upload.single('file'), (req, res) => {
-//   try {
-//     res.status(200).send('File uploaded successfully')
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })
-
-// const server = http.createServer(app);
-// const io = socketio(server);
-// io.on('connection', () => console.log('new websocket connected'))
-
 //Connection to db with mongoose
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
@@ -55,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   // Express serve up index.html file if it doesn't recognize routeget
   const path = require('path');
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
