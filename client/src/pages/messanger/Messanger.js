73eb3850350d69cc3e8850fd574c905b78337eb8
Dmitry Krepak <krepakdima@gmail.com>
2021-08-27 19:24:24 +0300

    .

--- client/src/pages/messanger/Messanger.js
+++ client/src/pages/messanger/Messanger.js
@@ -21,7 +21,9 @@ export default function Messanger() {
   const [currentChatReceiver, setCurrentChatReceiver] = useState(null);
   const [renderConversation, setRenderConversation] = useState(false);
   const messangerMode = useRef(true);
-  const socket = useRef(io("ws://localhost:8900"));
+  const socket = useRef(
+    io("ws://localhost:8900") || io("ws://socialmedia-dimkre.herokuapp.com/")
+  );
   const scrollRef = useRef();
 
   useEffect(() => {
